bbbf18a3b38841a4767a2dd3b9bf64735b7cab8a
Artem Teslenko <ates@ipv6.dp.ua>
2010-04-15 11:45:33 +0000

    Use = instead of ILIKE during 'login' comparison

--- schema.sql
+++ schema.sql
@@ -12,7 +12,7 @@ BEGIN
         LEFT OUTER JOIN assigned_radius_replies t2
             ON (t2.target_id = t0.id AND t2.target_type = 'Account')
         LEFT OUTER JOIN radius_replies t3 ON t3.id = t2.radius_reply_id
-    WHERE t0.login ILIKE $1 AND (t0.balance IS NOT NULL AND t0.balance > 0) AND t0.plan_id IS NOT NULL AND t0.active = TRUE;
+    WHERE t0.login = $1 AND (t0.balance IS NOT NULL AND t0.balance > 0) AND t0.plan_id IS NOT NULL AND t0.active = TRUE;
 END;
 $$ LANGUAGE plpgsql;
 
@@ -20,7 +20,7 @@ CREATE OR REPLACE FUNCTION iptraffic_start_session(VARCHAR, VARCHAR, VARCHAR, TI
 DECLARE
     acct_id INTEGER;
 BEGIN
-    SELECT id INTO acct_id FROM accounts WHERE login ILIKE $1;
+    SELECT id INTO acct_id FROM accounts WHERE login = $1;
     INSERT INTO iptraffic_sessions(account_id, ip, sid, started_at) VALUES (acct_id, $2, $3, $4);
 END;
 $$ LANGUAGE plpgsql;
@@ -30,7 +30,7 @@ DECLARE
     acct_id INTEGER;
     session_id INTEGER;
 BEGIN
-    SELECT id INTO acct_id FROM accounts WHERE login ILIKE $1;
+    SELECT id INTO acct_id FROM accounts WHERE login = $1;
     SELECT id INTO session_id FROM iptraffic_sessions WHERE sid = $2 AND finished_at IS NULL AND account_id = acct_id LIMIT 1;
     -- Don't update balance when session already closed
     IF FOUND THEN
@@ -47,7 +47,7 @@ $$ LANGUAGE plpgsql;
 CREATE OR REPLACE FUNCTION iptraffic_sync_session(VARCHAR, VARCHAR, TIMESTAMP, BIGINT, BIGINT, FLOAT) RETURNS INTEGER AS $$
 BEGIN
     UPDATE iptraffic_sessions SET octets_in = $4, octets_out = $5, updated_at = $3, amount = $6
-        WHERE sid = $2 AND account_id = (SELECT id FROM accounts WHERE login ILIKE $1);
+        WHERE sid = $2 AND account_id = (SELECT id FROM accounts WHERE login = $1);
     IF FOUND THEN
         RETURN 1;
     ELSE
